d87c7fe49ba6ca02283524aa6c75990124e68c3f
Vijayant Pandey <119390705+vijayant-pandey@users.noreply.github.com>
2025-10-03 22:16:01 +0530

    Update authService.js
    added the env route

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+// const API_URL = "http://localhost:5000/api/auth";
+const API_URL = `${process.env.REACT_APP_API_URL}/auth`;
 
 export const login = async (email, password) => {
   const res = await axios.post(`${API_URL}/login`, { email, password });
