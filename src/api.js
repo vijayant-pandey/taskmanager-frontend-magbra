f11e2522ff4e5e187c2c0e5af8e1789d92fce95c
Vijayant Pandey <119390705+vijayant-pandey@users.noreply.github.com>
2025-10-03 22:17:26 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend URL
+  // baseURL: "http://localhost:5000/api", // your backend URL
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const setAuthToken = (token) => {
